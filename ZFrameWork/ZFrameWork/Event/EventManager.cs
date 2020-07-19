e96912e815b4ce1e90f09eae87c2250d12fbe548
liuzhenhua <liuzhenhua12315@163.com>
2020-07-19 18:42:53 +0800

    envent事件demo

--- ZFrameWork/ZFrameWork/Event/EventManager.cs
+++ ZFrameWork/ZFrameWork/Event/EventManager.cs
@@ -6,9 +6,42 @@ using System.Threading.Tasks;
 
 namespace ZFrameWork.Event
 {
-    public class EventManager
+    public static class EventManager
     {
-        private readonly static Dictionary<string, List<Action>> evenDic = new Dictionary<string, List<Action>>();
+        private static readonly Dictionary<string, List<Delegate>> Listeners = new Dictionary<string, List<Delegate>>();
+
+
+        private static List<Delegate> GetHandlers(string eventId)
+        {
+            List<Delegate> handlers;
+            if (EventManager.Listeners.ContainsKey(eventId))
+            {
+                handlers = EventManager.Listeners[eventId];
+            }
+            else
+            {
+                handlers = new List<Delegate>();
+            }
+            return handlers;
+        }
+
+        public static void AddListener(string eventId, Action handler)
+        {
+            List<Delegate> handlers = EventManager.GetHandlers(eventId);
+            if (!handlers.Contains(handler))
+            {
+                handlers.Add(handler);
+            }
+        }
+
+        public static void AddListener<T>(string eventId, Action<T> handler)
+        {
+            List<Delegate> handlers = EventManager.GetHandlers(eventId);
+            if (!handlers.Contains(handler))
+            {
+                handlers.Add(handler);
+            }
+        }
 
     }
 }
