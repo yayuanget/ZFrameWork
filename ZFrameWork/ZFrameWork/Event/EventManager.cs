88458c0dfadf0f761df1c47cdb468ff876fa4563
liuzhenhua <liuzhenhua12315@163.com>
2020-07-19 17:00:58 +0800

    VS文件夹上传

---
+++ ZFrameWork/ZFrameWork/Event/EventManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZFrameWork.Event
+{
+    public class EventManager
+    {
+        private readonly static Dictionary<string, List<Action>> evenDic = new Dictionary<string, List<Action>>();
+
+    }
+}
